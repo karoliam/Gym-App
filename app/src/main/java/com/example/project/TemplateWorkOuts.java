1e7538a5c285551a471abeee6ed9821ca967eefa
karoliam <kmultas@gmail.com>
2021-12-08 15:51:00 +0200

    päivitin nyt sharedpreferencen toimimaan. eli tallentaa vaan tohon näkymään nyt noi numerot eikä lähetä niitä minnekkään.

--- app/src/main/java/com/example/project/TemplateWorkOuts.java
+++ app/src/main/java/com/example/project/TemplateWorkOuts.java
@@ -12,26 +12,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class TemplateWorkOuts extends AppCompatActivity {
     ArrayList<TemplateIntegers> templateIntegersArrayList;
     private TextView exerciseOneTV, exerciseTwoTV, exerciseThreeTV, exerciseFourTV, nameTV;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_template_work_outs);
-
-/*        Button saveButton = findViewById(R.id.saveButton);
-
+        loadData();
+        Button saveButton = findViewById(R.id.saveButton);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 saveData();
             }
-        });*/
+        });
 
         Bundle b = getIntent().getExtras();
         int i = b.getInt(TemplateActivity.EXTRA, 0);
@@ -46,19 +48,26 @@ public class TemplateWorkOuts extends AppCompatActivity {
         exerciseTwoTV.setText(TemplateSingleton.getInstance().getTemplate(i).getExercise2());
         exerciseThreeTV.setText(TemplateSingleton.getInstance().getTemplate(i).getExercise3());
         exerciseFourTV.setText(TemplateSingleton.getInstance().getTemplate(i).getExercise4());
+
     }
-/*
+
     private void saveData() {
-        SharedPreferences prefs = getSharedPreferences("saving templates", MODE_PRIVATE);
+        prefs = getSharedPreferences("saving templates", MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         Gson gson = new Gson();
         String json = gson.toJson(templateIntegersArrayList);
-        editor.putString ("templates", json);
+        editor.putString("templates", json);
         editor.apply();
     }
 
     private void loadData() {
-
-    }*/
-
+        prefs = getSharedPreferences("loading templates", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = prefs.getString("template loading", null);
+        Type type = new TypeToken<ArrayList<TemplateIntegers>>() {}.getType();
+        templateIntegersArrayList = gson.fromJson(json, type);
+        if (templateIntegersArrayList == null){
+            templateIntegersArrayList = new ArrayList<>();
+        }
+    }
 }
