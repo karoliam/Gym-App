ece2437f0d4a5e8f77140b295f9c814de5eefbc2
karoliam <kmultas@gmail.com>
2021-12-12 22:02:31 +0200

    Vaihdoin ArrayListin HashMapiksi niin kaikki liikkeet on eritelty

--- app/src/main/java/com/example/project/TemplateWorkOuts.java
+++ app/src/main/java/com/example/project/TemplateWorkOuts.java
@@ -2,8 +2,6 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -17,11 +15,10 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TemplateWorkOuts extends AppCompatActivity {
-    public ArrayList<Integer> weightArrayList;
-    public ArrayList<Integer> repArrayList;
-    public ArrayList<Integer> setArrayList;
+    public HashMap<String, Integer> templateHashmap;
     private TextView exerciseOneTV, exerciseTwoTV, exerciseThreeTV, exerciseFourTV, nameTV;
     private EditText weight1,weight2,weight3,weight4, set1, set2, set3, set4, rep1, rep2, rep3, rep4;
 
@@ -75,73 +72,44 @@ public class TemplateWorkOuts extends AppCompatActivity {
         rep4 = findViewById(R.id.repsEditText4);
 
 
-        weightArrayList.add(Integer.parseInt(weight1.getText().toString()));
-        weightArrayList.add(Integer.parseInt(weight2.getText().toString()));
-        weightArrayList.add(Integer.parseInt(weight3.getText().toString()));
-        weightArrayList.add(Integer.parseInt(weight4.getText().toString()));
-        setArrayList.add(Integer.parseInt(set1.getText().toString()));
-        setArrayList.add(Integer.parseInt(set2.getText().toString()));
-        setArrayList.add(Integer.parseInt(set3.getText().toString()));
-        setArrayList.add(Integer.parseInt(set4.getText().toString()));
-        repArrayList.add(Integer.parseInt(rep1.getText().toString()));
-        repArrayList.add(Integer.parseInt(rep2.getText().toString()));
-        repArrayList.add(Integer.parseInt(rep3.getText().toString()));
-        repArrayList.add(Integer.parseInt(rep4.getText().toString()));
+        templateHashmap.put("weight1", Integer.parseInt(weight1.getText().toString()));
+        templateHashmap.put("weight2", Integer.parseInt(weight2.getText().toString()));
+        templateHashmap.put("weight3", Integer.parseInt(weight3.getText().toString()));
+        templateHashmap.put("weight4", Integer.parseInt(weight4.getText().toString()));
+        templateHashmap.put("set1", Integer.parseInt(set1.getText().toString()));
+        templateHashmap.put("set2", Integer.parseInt(set2.getText().toString()));
+        templateHashmap.put("set3", Integer.parseInt(set3.getText().toString()));
+        templateHashmap.put("set4", Integer.parseInt(set4.getText().toString()));
+        templateHashmap.put("rep1", Integer.parseInt(rep1.getText().toString()));
+        templateHashmap.put("rep2", Integer.parseInt(rep2.getText().toString()));
+        templateHashmap.put("rep3", Integer.parseInt(rep3.getText().toString()));
+        templateHashmap.put("rep4", Integer.parseInt(rep4.getText().toString()));
 
     }
     //Tallennetaan Arraylist Sharedpreferenceihin onPausessa
     @Override
     public void onPause () {
         super.onPause();
-        SharedPreferences prefs = getSharedPreferences("weights", MODE_PRIVATE);
+        SharedPreferences prefs = getSharedPreferences("templates", MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
-        //weights
-        Gson weightGson = new Gson();
-        String weightJson = weightGson.toJson(weightArrayList);
-        editor.putString("weights", weightJson);
-        //Sets
-        Gson setGson = new Gson();
-        String setJson = setGson.toJson(setArrayList);
-        editor.putString("sets", setJson);
-        //Reps
-        Gson repGson = new Gson();
-        String repJson = repGson.toJson(repArrayList);
-        editor.putString("reps", repJson);
-
+        Gson Gson = new Gson();
+        String json = Gson.toJson(templateHashmap);
+        editor.putString("saving templates", json);
         editor.commit();
 
     }
     //Datan deserialisointi
     private void loadData() {
         //Weights
-        SharedPreferences prefs = getSharedPreferences("weights", MODE_PRIVATE);
-        Gson weightGson = new Gson();
-        String weightJson = prefs.getString("weights", null);
-        Type weightType = new TypeToken<ArrayList<Integer>>() {}.getType();
-        weightArrayList = weightGson.fromJson(weightJson, weightType);
+        SharedPreferences prefs = getSharedPreferences("templates", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = prefs.getString("saving templates", null);
+        Type type = new TypeToken<HashMap<String, Integer>>() {}.getType();
+        templateHashmap = gson.fromJson(json, type);
         //Jos lista on tyhjä, luodaan uusi tyhjä lista.
-        if (weightArrayList == null){
-            weightArrayList = new ArrayList<>();
+        if (templateHashmap == null){
+            templateHashmap = new HashMap<>();
         }
-        //Sets
-        SharedPreferences setPrefs = getSharedPreferences("sets", MODE_PRIVATE);
-        Gson setGson = new Gson();
-        String setJson = setPrefs.getString("sets", null);
-        Type setType = new TypeToken<ArrayList<Integer>>() {}.getType();
-        setArrayList = setGson.fromJson(setJson, setType);
-        //Jos lista on tyhjä, luodaan uusi tyhjä lista.
-        if (setArrayList == null){
-            setArrayList = new ArrayList<>();
-        }
-        //Reps
-        SharedPreferences repPrefs = getSharedPreferences("reps", MODE_PRIVATE);
-        Gson repGson = new Gson();
-        String repJson = repPrefs.getString("reps", null);
-        Type repType = new TypeToken<ArrayList<Integer>>() {}.getType();
-        repArrayList = repGson.fromJson(repJson, repType);
-        //Jos lista on tyhjä, luodaan uusi tyhjä lista.
-        if (repArrayList == null){
-            repArrayList = new ArrayList<>();
-        }
+
     }
 }
