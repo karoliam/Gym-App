e2274382e9f44d1805f65afe108ab90914eeea1b
larppiiis <laura.immonen@metropolia.fi>
2021-12-12 23:02:24 +0200

    Siistitty. Voi tallentaa yhden oman treenin kerrallaan.

--- app/src/main/java/com/example/project/TemplateActivity.java
+++ app/src/main/java/com/example/project/TemplateActivity.java
@@ -3,8 +3,6 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
